8c370b544f7eac960c4f79c3ba265450093abfee
Miles <133746195+milesxyz@users.noreply.github.com>
2023-08-02 14:47:45 +0800

    feature: add Ownable2Step and Rescuable

---
+++ contracts/access/Ownable2Step.sol
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022, Circle Internet Financial Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./Ownable.sol";
+
+/**
+ * @dev forked from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7c5f6bc2c8743d83443fa46395d75f2f3f99054a/contracts/access/Ownable2Step.sol
+ * Modifications:
+ * 1. Update Solidity version from 0.8.0 to 0.7.6. Version 0.8.0 was used
+ * as base because this contract was added to OZ repo after version 0.8.0.
+ *
+ * Contract module which provides access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership} and {acceptOwnership}.
+ *
+ * This module is used through inheritance. It will make available all functions
+ * from parent (Ownable).
+ */
+abstract contract Ownable2Step is Ownable {
+    address private _pendingOwner;
+
+    event OwnershipTransferStarted(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Returns the address of the pending owner.
+     */
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    /**
+     * @dev Starts the ownership transfer of the contract to a new account. Replaces the pending transfer if there is one.
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual override onlyOwner {
+        _pendingOwner = newOwner;
+        emit OwnershipTransferStarted(owner(), newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`) and deletes any pending owner.
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual override {
+        delete _pendingOwner;
+        super._transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev The new owner accepts the ownership transfer.
+     */
+    function acceptOwnership() external {
+        address sender = _msgSender();
+        require(pendingOwner() == sender, "Ownable2Step: caller is not the new owner");
+        _transferOwnership(sender);
+    }
+}
---
+++ contracts/access/Rescuable.sol
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022, Circle Internet Financial Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./Ownable2Step.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @notice Base contract which allows children to rescue ERC20 locked in their contract.
+ * @dev Forked from https://github.com/centrehq/centre-tokens/blob/0d3cab14ebd133a83fc834dbd48d0468bdf0b391/contracts/v1.1/Rescuable.sol
+ * Modifications:
+ * 1. Update Solidity version from 0.6.12 to 0.7.6 (8/23/2022)
+ */
+contract Rescuable is Ownable2Step {
+    using SafeERC20 for IERC20;
+
+    address private _rescuer;
+
+    event RescuerChanged(address indexed newRescuer);
+
+    /**
+     * @notice Returns current rescuer
+     * @return Rescuer's address
+     */
+    function rescuer() external view returns (address) {
+        return _rescuer;
+    }
+
+    /**
+     * @notice Revert if called by any account other than the rescuer.
+     */
+    modifier onlyRescuer() {
+        require(msg.sender == _rescuer, "Rescuable: caller is not the rescuer");
+        _;
+    }
+
+    /**
+     * @notice Rescue ERC20 tokens locked up in this contract.
+     * @param tokenContract ERC20 token contract address
+     * @param to        Recipient address
+     * @param amount    Amount to withdraw
+     */
+    function rescueERC20(
+        IERC20 tokenContract,
+        address to,
+        uint256 amount
+    ) external onlyRescuer {
+        tokenContract.safeTransfer(to, amount);
+    }
+
+    function rescueNative(
+        address to,
+        uint256 amount
+    ) external onlyRescuer {
+        (bool success,) = to.call{value:amount}(new bytes(0));
+        require(success, 'Rescuable: native asset transfer failed');
+    }
+
+    /**
+     * @notice Assign the rescuer role to a given address.
+     * @param newRescuer New rescuer's address
+     */
+    function updateRescuer(address newRescuer) external onlyOwner {
+        require(
+            newRescuer != address(0),
+            "Rescuable: new rescuer is the zero address"
+        );
+        _rescuer = newRescuer;
+        emit RescuerChanged(newRescuer);
+    }
+}
