f7be283d5c73fc26941fc173e54b0939b844bfef
lucasJackson0 <133747837+lucasJackson0@users.noreply.github.com>
2023-08-31 15:22:20 +0800

    improve: code optimization

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,11 +1,3 @@
-export const enum CHAINID {
-  Ethereum = 1,
-  BNBChain = 56,
-  Polygon = 137,
-
-  EthereumGoerli = 5,
-}
-
 export enum NETWORKS {
   Mainnet = 'MainNet',
   TestNet = 'TestNet',
